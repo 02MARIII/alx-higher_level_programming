20eeef067b0b269e1224e03f1d40909f77fa4b21
02MARIII <MARISHHHALLL@gmail.com>
2023-11-30 16:58:20 +0100

    change import method

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    add_import = __import__("add_0")
+    from add_0 import add
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add_import.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
