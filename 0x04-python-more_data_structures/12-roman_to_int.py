526f2c569f5018a4c4629171828aac9cda942dea
02MARIII <MARISHHHALLL@gmail.com>
2023-12-07 00:53:11 +0100

    add new task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    prev_value = 0
+
+    if isinstance(roman_string, str) and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            current_value = val[roman_string[c]]
+            if current_value >= prev_value:
+                res += current_value
+            else:
+                res -= current_value
+    return res
