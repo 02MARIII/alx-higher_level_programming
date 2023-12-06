45a5eb350ca910710a815748da68d512357540ea
02MARIII <MARISHHHALLL@gmail.com>
2023-12-07 00:30:39 +0100

    add new task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    scoreValue = float('-inf')
+    scoreKey = None
+    for key, value in a_dictionary.items():
+        if isinstance(value, int) and value > scoreValue:
+            scoreValue = value
+            scoreKey = key
+    return scoreKey
