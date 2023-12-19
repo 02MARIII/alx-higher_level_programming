14191bcb154d62e45bc945f330344c54e979c3c1
02MARIII <MARISHHHALLL@gmail.com>
2023-12-19 18:11:28 +0100

    add new task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """define a square."""
+    def __init__(self, size):
+        """initializing."""
+        self.__size = size
